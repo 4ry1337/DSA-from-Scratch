9d3e4d6bf96af6223f987d176cceb3cf41ea7504
rakhat <yskak.rakhat@gmail.com>
2026-06-29 03:01:06 +0500

    feat: deque

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,2 @@
-#include "list.h"
-#include <iostream>
 
-int main() {
-    dsa::List<int> list;
-    std::cout << sizeof(list);
-    list.push_back(12);
-    list.push_back(13);
-    list.push_back(145);
-    return 0;
-}
+int main() { return 0; }
