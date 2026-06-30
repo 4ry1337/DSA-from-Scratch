ba4f38fa6d2b2fa86ba38a63e6ea4d088cb72e05
rakhat <yskak.rakhat@gmail.com>
2026-06-30 08:33:12 +0500

    feat: stl find

---
+++ src/find.h
@@ -0,0 +1,16 @@
+#ifndef FIND_H_
+#define FIND_H_
+
+namespace dsa {
+template <typename Iterator, typename Item>
+Iterator find(Iterator start, Iterator end, const Item &key) {
+    for (Iterator it = start; it != end; ++it) {
+        if (*it == key) {
+            return it;
+        }
+    }
+    return end;
+};
+} // namespace dsa
+
+#endif // !FIND_H_
